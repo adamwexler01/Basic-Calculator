341fb424e2f71a9c0896c1ccd114dfeb0da4e95a
Adam Wexler <adamwexler01@gmail.com>
2015-12-29 13:29:31 -0500

    Initial Commit
    Adding in the basic.js file

---
+++ basic.js
@@ -0,0 +1,31 @@
+/*
+
+
+
+
+
+
+
+*/
+
+
+
+function addition(operand1, operand2){
+	return operand1 + operand2;
+}
+
+
+function calculator(){
+	var choice;
+
+	console.log("Welcome to the Basic Four-Function Calculator");
+
+	while(choice != 4){
+		console.log("Please Select An Option");
+		console.log("1. Additon");
+		console.log("2. Subtraction");
+	}
+
+
+
+}
